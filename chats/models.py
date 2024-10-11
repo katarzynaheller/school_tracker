3ceef58c9361c0d72505c1d60744c63de509967f
kathe <katarzynaheller1@gmail.com>
2024-10-11 14:50:05 +0200

    Revert related_name change

--- chats/models.py
+++ chats/models.py
@@ -8,22 +8,21 @@ class Message(models.Model):
     sender = models.ForeignKey(
         CustomUser,
         on_delete=models.CASCADE,
-        related_name='sender',
+        related_name='sent_messages',
     )
-
     child = models.ForeignKey(Child, on_delete=models.CASCADE, null=True, blank=True)
     message_text = models.CharField(max_length=1000)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
+        return f"Message about {child.full_name} sent by {sender.email}"
         try:
             child_group = self.child.group_member if self.child else "No Group assigned"
             return f"Message about {self.child}(Group {child_group})"
         except Group.DoesNotExist:
         # Handle the case when the group does not exist
             return f"Message about {self.child} (no assigned group)"
-    
-    #the most recent messsages appearing first    
+
     class Meta:
         ordering = ('-timestamp',)
 
