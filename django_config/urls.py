8a1b0d2140c10dba2296d118780797726d142960
kathe <katarzynaheller1@gmail.com>
2024-11-06 10:51:45 +0100

    Refactor: add endpont for swagger, delete schema file from repository

--- django_config/urls.py
+++ django_config/urls.py
@@ -3,12 +3,15 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from drf_spectacular.views import SpectacularAPIView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView
+)
 
 from school_tracker.accounts.views import UserViewSet
 from school_tracker.chats.views import MessageViewSet
-from school_tracker.schedules.views import DayPlanViewSet
 from school_tracker.members.views import MembersViewSet
+from school_tracker.schedules.views import DayPlanViewSet
 
 
 router = routers.SimpleRouter()
@@ -22,10 +25,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema")
-    # path("api/v1/chats/", include("school_tracker.chats.urls")),
-    # path("api/v1/dayplans/", include("school_tracker.schedules.urls")),
-    # path("api/v1/members/", include("school_tracker.members.urls")), 
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("swagger/", SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui')
 ]
 
 app_name="api"
